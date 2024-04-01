02cd8d6d751c9dfc3376ce34b8df424e86b65f27
Sripathy R S <123567370+sripathy914@users.noreply.github.com>
2024-04-01 21:44:12 +0530

    TwoD-Arrays

--- TwoD-Arrays.sol
+++ TwoD-Arrays.sol
@@ -1,17 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract TwoD-Arrays {
+contract TwoDArrays
+{
     uint[] public uintArray = [1, 2, 31];
     string[] public stringArray = ["apple", "banana", "carrot"];
     string public value;
     uint[][] public TwoDarrays=[[1,2,3],[4,5,6]];
 
-
-  //dont confuse with below code, just take it for reference.
-    function addValue(string memory _value) public {
-        values.push(_value);
-    }
-
-    function valueCount() public view returns (uint) {
-        return values.length
+}
