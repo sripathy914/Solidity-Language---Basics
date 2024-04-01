8392e3ee9ad2a238f06bd1444bb6f4213b504ef3
Sripathy R S <123567370+sripathy914@users.noreply.github.com>
2024-04-01 22:21:29 +0530

    MappingandStructure

---
+++ MappingandStructure.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Mapping_Structure
+{
+    mapping(uint => Comics) public comics;
+    
+    struct Comics
+    {
+        string title;
+        string author;
+    }
+
+    function addcomics(uint _id, string memory _title, string memory _author) public
+    {
+        comics[_id]=Comics(_title, _author); 
+    }
+}
+
+---------------------------------------------------
+//Without constructo
+--------------------------------------------------
