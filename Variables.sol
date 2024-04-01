90122be32bf227f5b58e45fc187be12c5d545f64
Sripathy R S <123567370+sripathy914@users.noreply.github.com>
2024-04-01 14:24:58 +0530

    basic variable datatypes

---
+++ Variables.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: Unlicense
+
+pragma solidity ^0.8.0;
+
+contract Variables 
+{
+    //String declaration
+    string public myString = "Solidity";
+    
+    //boolean declaration
+    bool public boolean = true;
+    
+    //unsigned integer declaration
+    uint public myUint = 1;
+
+    //integer declartion
+    int public myInt = 1;
+    
+    //address declaration
+    address public myAddress = 0x52bc44d5378309EE2abF1539BF71dE1b7d7bE3b5;
+}
+
+//Always remember the file name should be same as contract name
