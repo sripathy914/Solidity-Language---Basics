76742290439e4a28b32df34df99f8479d82878a9
Sripathy R S <123567370+sripathy914@users.noreply.github.com>
2024-04-01 19:08:36 +0530

    OneD-Arrays

---
+++ OneD-Arrays.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract MyContract {
+    uint[] public uintArray = [1, 2, 31];
+    string[] public stringArray = ["apple", "banana", "carrot"];
+    string public value;
+    string[] public values;
+
+    function addValue(string memory _value) public {
+        values.push(_value);
+    }
+
+    function valueCount() public view returns (uint) {
+        return values.length
