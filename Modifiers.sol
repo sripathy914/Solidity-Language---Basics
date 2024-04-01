5373a8f4851fb604966657dd0bdda57ffd2813ff
Sripathy R S <123567370+sripathy914@users.noreply.github.com>
2024-04-01 14:42:16 +0530

    Access Modifiers for variables

---
+++ Modifiers.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: Unlicense
+
+pragma solidity ^0.8.0;
+
+contract MyContract 
+{
+    string namel = "Name 1";
+    string private name2 = "Name 2";
+    string internal name3 = "Name 3";
+    string public name4 = "Name 4";
+}
