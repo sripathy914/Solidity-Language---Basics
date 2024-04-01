2d851bbbf0e75a2e33ff9f101d4ba5db6fc3cc47
Sripathy R S <123567370+sripathy914@users.noreply.github.com>
2024-04-01 19:24:23 +0530

    Mapping

--- Mapping.sol
+++ Mapping.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.0;
+
+contract Mapping
+{
+    //mapping(key => value ) variable_name;
+
+    mapping(uint => string) names;
+
+    constructor() public
+    {
+        names[1] = "Adam";
+        names[2] = "Bruce";
+        names[3] = "Carl";
+    }
+}
